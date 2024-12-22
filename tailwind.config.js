a4902492d62b5d23558c2acd10f36e703251f404
Baldeabdoullah <abdoullahbalde@gmail.com>
2024-12-22 11:00:53 +0000

    commit colors

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        vuejs: {
+          100: "#7deb65",
+          200: "#6ff252",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
